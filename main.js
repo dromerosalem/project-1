a3c034cd9785885a7db8e0b6a5bd8e9bee00f9a4
dromerosalem <58950426+dromerosalem@users.noreply.github.com>
2020-02-26 11:12:59 +0000

    Start button is not being recalled

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function setupGame(){
   
   const cellsXcells = 20
   let ship = 389
-  let shoots = 0
+  
   // let bombs = 0
   const totalGrid = cellsXcells * cellsXcells
   const grid = document.querySelector('.grid')
@@ -29,173 +29,162 @@ function setupGame(){
     
 
   }
+
+  let start = true
+
+ 
+ 
+
+
   //INVAERS MOVEMENT WHAT ARE NOT MOVING DOWN
+  
+  
+    
   button.addEventListener('click',() => {
-
-    // 1) Have a single setInterval, that loops/foreeaches through invaders and moves them
-    // 2) When a rocket hits an invader, clear the interval, and remove that invader from the array
-    // 3) Start your interval again, with one less invader.
-
-  
-    setInterval(() => {
-
-      for (let i = 0; i < invaders.length; i++) {
-        if (alienDirection === 'right') {
-          
-          if (rightWall.includes(invaders[i])) {
-            for (let i = invaders.length - 1; i >= 0; i--) {
-              cells[invaders[i]].classList.remove('invader')
-              invaders[i] += cellsXcells
-              cells[invaders[i]].classList.add('invader')       
-            }
-            return alienDirection = 'left' 
-          } else {
-            cells[invaders[i]].classList.remove('invader')
-            invaders[i] += invaderMove
-            cells[invaders[i]].classList.add('invader')
-          }
-        } //else if (alienDirection === 'left') {
-        //   if (leftWall.includes(invaders[i])) {
-        //     for (let i = 0; i < invaders.length; i++) {
-        //       cells[invaders[i]].classList.remove('invader')
-              
-        //     }
-        //     for (let i = 0; i < invaders.length; i++) {
-        //       invaders[i] += cellsXcells 
-        //       cells[invaders[i]].classList.add('invader')
-        //     }
-            
-        //     alienDirection = 'right' 
-        //   } else {
-        //     cells[invaders[i]].classList.remove('invader')
-        //     invaders[i] -= invaderMove
-        //     cells[invaders[i]].classList.add('invader')
-        //   }
-        // } 
-      }
-      // cells[invader].classList.add('invader')    
-  
-    }, 500)
-
     
-    
-    const generateBomb = setInterval(() => { 
-      let bomb = 0
-      const invaderFront = invaders.slice(-4)
-      const nuke = (Math.floor(Math.random() * invaderFront.length))
-      bomb = invaderFront[nuke] += cellsXcells
-      
-      
-      const dropBombInterval = setInterval(() => {
-        cells[bomb].classList.remove('bomb')
-        bomb += cellsXcells
-        cells[bomb].classList.add('bomb')
+    button.blur()
+
+    if (start === true){
+      start = false
         
-      }, 200)
-      // bombs = invaderFront[nuke] + cellsXcells
-      // cells[bombs].classList.add('bomb')
-      // bombs += cellsXcells
-      // cells[bombs].classList.remove('bomb')
+        
+       
+      //invaders movement
+      
+      setInterval(() => {
 
-      // cells[invaderFront[nuke]].classList.remove('bomb')
-      // cells[invaderFront[nuke]].classList.add('bomb')
-      
-      
-    }, 2000)
+        for (let i = 0; i < invaders.length; i++) {
+          if (alienDirection === 'right') {
+          
+            if (rightWall.includes(invaders[i])) {
+              for (let i = invaders.length - 1; i >= 0; i--) {
+                cells[invaders[i]].classList.remove('invader')
+                invaders[i] += cellsXcells
+                cells[invaders[i]].classList.add('invader')       
+              }
+              return alienDirection = 'left' 
+            } else {
+              cells[invaders[i]].classList.remove('invader')
+              invaders[i] += invaderMove
+              cells[invaders[i]].classList.add('invader')
+            }
+          } else if (alienDirection === 'left') {
+            if (leftWall.includes(invaders[i])) {
+              for (let i = 0; i < invaders.length; i++) {
+                cells[invaders[i]].classList.remove('invader')
+              
+              }
+              for (let i = 0; i < invaders.length; i++) {
+                invaders[i] += cellsXcells 
+                cells[invaders[i]].classList.add('invader')
+              }
+            
+              alienDirection = 'right' 
+            } else {
+              cells[invaders[i]].classList.remove('invader')
+              invaders[i] -= invaderMove
+              cells[invaders[i]].classList.add('invader')
+            }
+          } 
+        }
+        // cells[invader].classList.add('invader')    
+  
+      }, 500)
+
     
-  })
-
+      //bombs generate
+      const generateBomb = setInterval(() => { 
+        let bomb = 0
+        const invaderFront = invaders.slice(-4)
+        const nuke = (Math.floor(Math.random() * invaderFront.length))
+        bomb = invaderFront[nuke] += cellsXcells
+      
+      
+        const dropBombInterval = setInterval(() => {
+          if (bomb >= 380){
+            cells[bomb].classList.remove('bomb')
+            clearInterval(dropBombInterval)
+          
+          } else {
+            cells[bomb].classList.remove('bomb')
+            bomb += cellsXcells
+            cells[bomb].classList.add('bomb')
+            console.log(cells[bomb])
+            console.log(bomb)
+          }
+        
+        }, 200)
+            
+      }, 2000)
+      cells[ship].classList.add('ship')
+      
+      //SHIP CODE
+      
   
 
-  cells[ship].classList.add('ship')
-  //SHIP CODE
-  function shoot(){
-    
+
+      document.addEventListener('keydown', (event) => {
+        // console.log(event)
+        if (event.key === 'ArrowRight') {
+          if (ship === cells.length - 1) {
+            return
+          }
+          cells[ship].classList.remove('ship')
+          ship += 1
+          cells[ship].classList.add('ship')
+        } else if (event.key === 'ArrowLeft') {
+          if (ship === 380) {
+            return
+          }
+          cells[ship].classList.remove('ship')
+          ship -= 1
+          cells[ship].classList.add('ship')
+
+        } else if (event.code === 'Space'){
+          shoot()
+        }
+      
+      })
+    }
+  })
+  function shoot() {
+    let shoots = 0
     // shoots = ship - cellsXcells
     // shoots = shoots - cellsXccells
-    
+
     shoots = ship - cellsXcells
     cells[shoots].classList.add('shoot')  
 
     // 
-  
+
     const shootsMovement = setInterval(() => {
 
       console.log(shoots)
-      // if (shootMove === true){
-      //   invaders.forEach(invader => {
-      //     if (invader === shoots) {
-      cells[shoots].classList.remove('shoot')
-      shoots -= 20
-      cells[shoots].classList.add('shoot')  
-
-      if (cells[shoots].classList.contains('invader') === true) {    
-        //  const elem = invaders.length
-        invaders.splice(invaders.indexOf(cells[shoots]))
-        console.log(invaders)    
-        cells[shoots].classList.remove('invader')
-        cells[shoots].classList.remove('shoot')
+      if (shoots <= 19){
         clearInterval(shootsMovement)
+        cells[shoots].classList.remove('shoot')
+      } else {
+
+        cells[shoots].classList.remove('shoot')
+        shoots -= 20
+        cells[shoots].classList.add('shoot')  
+
+        if (cells[shoots].classList.contains('invader') === true) {    
+          //  const elem = invaders.length
+          invaders.splice(invaders.indexOf(cells[shoots]))
+          console.log(invaders)    
+          cells[shoots].classList.remove('invader')
+          cells[shoots].classList.remove('shoot')
+          clearInterval(shootsMovement)
+        }
+  
       }
     
-            
-      // clearInterval(shootsMovement)
-      // shootMove = false
-      // invaders.splice( invaders.indexOf(invader), 1 )
-      // console.log(invaders)
-      // cells[invader].classList.remove('shoot')
-      // console.log('hello evryone')
-      // cells[invader].classList.remove('invader')
-                  
-      // }
-      // })
-       
-      // if (shoots === 0) {
-      // shoots = ship - cellsXcells    
-      // cells[shoots].classList.add('shoot')  
-      // } else  {
-      //   cells[shoots].classList.remove('shoot')
-      //   shoots = shoots - cellsXcells
-      //   cells[shoots].classList.add('shoot')  
-      // }
-      // } 
-     
-      
-      // if (cells.contains('invader') && cells.contains('shoot')){
-      //   cells[shoots].classList.remove('shoot')
-      //   cells[invaders].classList.remove('invader')
-      // }
     }, 500)
   }
   
-
-
-  document.addEventListener('keydown', (event) => {
-    // console.log(event)
-    if (event.key === 'ArrowRight') {
-      if (ship === cells.length - 1) {
-        return
-      }
-      cells[ship].classList.remove('ship')
-      ship += 1
-      cells[ship].classList.add('ship')
-    } else if (event.key === 'ArrowLeft') {
-      if (ship === 380) {
-        return
-      }
-      cells[ship].classList.remove('ship')
-      ship -= 1
-      cells[ship].classList.add('ship')
-
-    } else if (event.code === 'Space'){
-      shoot()
-    }
-      
-  })
-
   
-
-
+  
   
 }
 
